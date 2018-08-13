796f11f9c0e78e339c7101d70df9d57e9db47731
mesoor <zhouzhongyi666@gmail.com>
2018-08-13 13:29:05 +0800

    0813: finish sliding windows

--- config.py
+++ config.py
@@ -3,5 +3,11 @@ tb_dir = './tensorboard'
 
 ckpt_dir = './checkpoints'
 
-h_wind = 30
-w_wind = 30
+h_sample_rate = 30
+w_sample_rate = 30
+
+blur_img_dir = './blur_img'
+save_print = 30
+# h_wind = 30
+# w_wind = 30
+
